45d3bbfaf9059af9dc909fc43513e31450f3c4ec
Iqbal Khan <iqbalbacha73@gmail.com>
2020-12-04 06:01:05 -0700

    my first cpp program

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+       string virusname = "corona";
+      //  this string is used for coroona
+
+      int year = 2021 ;
+      // in int we only store numbers i.e 1,3,4,5,6,7 and so on
+ 
+    cout<< "there was a virus called "  << virusname <<  " virus "<<endl;
+
+    cout<<"thats was in the year of " << year <<endl;
+
+    virusname= "china";
+    // this is aother name of virus withon the same string
+
+    cout<< virusname<< " caused very financial problems in Pakistan"<<endl;
+
+    cout<<"the PM of Pakistan in " <<year<< " was  imran khan"<<endl;
+
+  return 0;
+
+}
+
