5a37847a5d1f763f06456d9358e3009c9ce5f3bd
Iqbal Khan <iqbalbacha73@gmail.com>
2020-12-07 02:24:50 -0700

    added strings

--- main.cpp
+++ main.cpp
@@ -4,22 +4,25 @@ using namespace std;
 int main(){
 
        string virusname = "corona";
-      //  this string is used for coroona
+           //  this string is used for coroona
 
       int year = 2021 ;
-      // in int we only store numbers i.e 1,3,4,5,6,7 and so on
+            // in int we only store numbers i.e 1,3,4,5,6,7 and so on
  
-    cout<< "there was a virus called "  << virusname <<  " virus "<<endl;
+    cout<< "there was a virus called "  << virusname <<  " virus\n";
 
     cout<<"thats was in the year of " << year <<endl;
 
-    virusname= "china";
-    // this is aother name of virus withon the same string
+    virusname= "corona";
+            // this is aother name of virus withon the same string
 
-    cout<< virusname<< " caused very financial problems in Pakistan"<<endl;
+    cout<< virusname[0]<< " caused very financial problems in Pakistan\n";
 
-    cout<<"the PM of Pakistan in " <<year<< " was  imran khan"<<endl;
-      cout<<"hello world";
+    cout<<"the PM of Pakistan in " <<year<< " was  imran khan\n";
+
+
+
+
   return 0;
 
 }
